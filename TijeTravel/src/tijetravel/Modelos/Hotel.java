f065ea4f2d3d3aa4e24fb7a49c381a7226c3772d
migmik <miguel.pereyra@alu.inspt.utn.edu.ar>
2026-07-08 09:52:14 -0300

    Agregar archivos iniciales del proyecto TijeTravel, incluyendo algo de estructura y clases de modelo

---
+++ TijeTravel/src/tijetravel/Modelos/Hotel.java
@@ -0,0 +1,66 @@
+public class Hotel {
+    private int codigo;
+    private String nombre;
+    private String direccion;
+    private String ciudad;
+    private String telefono;
+    private int plazasDisponibles;
+
+    public Hotel(int codigo, String nombre, String direccion, String ciudad, String telefono, int plazasDisponibles) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.direccion = direccion;
+        this.ciudad = ciudad;
+        this.telefono = telefono;
+        this.plazasDisponibles = plazasDisponibles;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getCiudad() {
+        return ciudad;
+    }
+
+    public void setCiudad(String ciudad) {
+        this.ciudad = ciudad;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public int getPlazasDisponibles() {
+        return plazasDisponibles;
+    }
+
+    public void setPlazasDisponibles(int plazasDisponibles) {
+        this.plazasDisponibles = plazasDisponibles;
+    }
+
+}
---
+++ TijeTravel/src/tijetravel/Modelos/Turista.java
@@ -0,0 +1,10 @@
+public class Turista {
+    private int codigo;
+    private String nombre;
+    private String apellido;
+    private String direccion;
+    private String email;
+    private String telefonoFijo;
+    private String telefonoCelular;
+
+}
